823f3435d13d5b6581a697e6ceef389469df0560
ReinierH <r.m.hasper@gmail.com>
2017-04-04 09:34:37 +1200

    added ir driver

---
+++ lib/irreceiver.c
@@ -0,0 +1,5 @@
+#include "irreceiver.h"
+
+
+uint8_t irreceiver_get_data(uint8_t *data, size_t size) {
+}
---
+++ lib/irreceiver.h
@@ -0,0 +1,12 @@
+#ifndef IRRECEIVER_H
+#define IRRECEIVER_H
+
+#include <string.h>
+#include <inttypes.h>
+
+typedef struct {
+} irreceiver_t;
+
+uint8_t irreceiver_get_data(uint8_t *data, size_t size);
+
+#endif
